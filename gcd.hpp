089320373a3af41b5823342514e254520b10a956
evan <gmziven@gmai.com>
2018-09-14 15:14:11 -0700

    init

---
+++ gcd.hpp
@@ -0,0 +1,16 @@
+//
+// Created by evan on 2018/9/14.
+//
+
+#ifndef HELLOCLION_GCD_HPP
+#define HELLOCLION_GCD_HPP
+
+#endif //HELLOCLION_GCD_HPP
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd ( const int a, const int b);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "gcd.hpp"
 
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd ( const int a, const int b){
+    int temp = a;
+    while(temp>0){
+        if(a%temp==0&&b%temp==0)
+            return temp;
+        temp--;
+    }
+
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << gcd(35,20) << std::endl;
+
     return 0;
 }
